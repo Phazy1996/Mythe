917f70174838c3ee765c2536f30b7fe9c0361bdd
Phineas Moore <phineasmoore@gmail.com>
2016-02-15 11:56:49 +0100

    15-02-2016 Phineas Game Pauses

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,14 +2,43 @@
 using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
+    [SerializeField]
+    private bool isPaused = false;
+    public GameObject pauseMenuCanvas;
+    public GameObject pauseButtonCanvas;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+       Pause();
 	}
+
+    void Pause() {
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            pauseButtonCanvas.SetActive(false);
+            Time.timeScale = 0f;
+        }
+        else {
+            pauseMenuCanvas.SetActive(false);
+            pauseButtonCanvas.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            Time.timeScale = 1f;
+        }
+    }
+
+    void Resume() {
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void PauseButton()
+    {
+        isPaused = true;
+    }
+
 }
