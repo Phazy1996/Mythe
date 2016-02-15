9ceb122ccc6ad1361cdcfb8686fa6f646457da62
Sebastiaan Kappert <19070@ma-web.nl>
2016-02-15 12:16:51 +0100

    enemyspawner bug fix

--- Assets/Scripts/SpawnSystem/enemySpawner.cs
+++ Assets/Scripts/SpawnSystem/enemySpawner.cs
@@ -23,16 +23,22 @@ public class enemySpawner : MonoBehaviour {
     private int _SpawnDist;
     //ints
 
+    //bools
+    private bool _enemiesSpawned;
+    //bools
     void Start() {
+        _enemiesSpawned = false;
         _spawnLocation = transform.FindChild(GameTags.enemySpawnPoint);
         _enemyWaveLength = _enemyWave.Count;
     }
     private void OnTriggerEnter2D(Collider2D other)  {
-        if (other.tag == GameTags.player){
-            Debug.Log("spawn enemies");
+        if (other.tag == GameTags.player && _enemiesSpawned == false){
             EnemyPlacer();
+            _enemiesSpawned = true;
         }
     }
+
+
     //the code that will actually place the enemies on screen
     private void EnemyPlacer() {
         for (int i = 0; i < _enemyWaveLength; i++)
