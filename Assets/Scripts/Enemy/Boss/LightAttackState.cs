5ee1e995f3780f404df7d5046996ddccf1664014
Wopsie <joaghim.beltman@gmail.com>
2016-03-10 13:45:29 +0100

    boss commit before merge

--- Assets/Scripts/Enemy/Boss/LightAttackState.cs
+++ Assets/Scripts/Enemy/Boss/LightAttackState.cs
@@ -10,6 +10,7 @@ public class LightAttackState : State {
     private Vector2 _DestPoint;
 
     private bool _OnLeftSide;
+    private bool _DoneAttacking = false;
 
     private GameObject _Camera;
 
@@ -17,27 +18,85 @@ public class LightAttackState : State {
 
     public override void Enter()
     {
-        Debug.Log("LightAttack");
         _BossRenderer = GetComponent<SpriteRenderer>();
         _Camera = GameObject.FindWithTag("MainCamera");
         CheckSide();
+
+        //set startpoint where the boss was positioned when he entered this state
+        _StartPoint.x = transform.position.x;
+        //set destination point where the boss needs to walk for his light attack
+        if (_OnLeftSide)
+            _DestPoint.x = _StartPoint.x + 3;
+        else
+            _DestPoint.x = _StartPoint.x - 3;
+
+        Debug.Log("Destination: " + _DestPoint + ". StartPoint: " + _StartPoint);
     }
 
     public override void Act()
     {
-        if (_OnLeftSide == true){
-            Debug.Log("attack to right");
+        /*
+        if (_OnLeftSide == true && _DoneAttacking == false)
+        {
             WalkForwardR();
-        }  
-        else{
-            Debug.Log("attack to left");
+            _StartPoint.x = transform.position.x;
+        }else if(_OnLeftSide == false && _DoneAttacking == false)
+        {
             WalkForwardL();
-        }     
+            _StartPoint.x = transform.position.x;
+        }else if (_OnLeftSide == false &&_DoneAttacking == true)
+        {
+            //run walk back right
+            WalkBackR();
+        }else if(_OnLeftSide == true && _DoneAttacking == true)
+        {
+            WalkBackL();
+        }
+         * */
+
+        if(_DoneAttacking == false)
+        {
+            //start walking to center
+            WalkToCenter();
+        }else if(_DoneAttacking == true)
+        {
+            WalkBack();
+        }
     }
 
-    public override void Reason()
-    {
 
+    void WalkToCenter()
+    {
+        //make boss walk to the destination point
+        if(_OnLeftSide == true && transform.position.x < _DestPoint.x)
+        {
+            transform.Translate(_WalkRight * Time.deltaTime);
+            Debug.Log("walk right to centre");
+        }else if(_OnLeftSide == false && transform.position.x > _DestPoint.x)
+        {
+            transform.Translate(_WalkLeft * Time.deltaTime);
+            Debug.Log("walk left to centre");
+        }
+
+        if (transform.position.x >= _DestPoint.x && _OnLeftSide == true)
+        {
+            Attack();
+        }
+        else if (transform.position.x <= _DestPoint.x && _OnLeftSide == false)
+            Attack();
+    }
+
+    void WalkBack()
+    {
+        Debug.Log("Now Walking Back");
+        //make boss move back to the start point
+        if(transform.position.x > _StartPoint.x)
+        {
+            transform.Translate(_WalkLeft * Time.deltaTime);
+        }else if(transform.position.x < _StartPoint.x)
+        {
+            transform.Translate(_WalkRight * Time.deltaTime);
+        }
     }
 
     void CheckSide()
@@ -48,56 +107,16 @@ public class LightAttackState : State {
             _OnLeftSide = true;
     }
 
-    void WalkForwardL()
-    {
-        //make boss step to the left and then call attack
-        if(transform.position.x >= (_StartPoint.x +2f))
-        {
-            transform.Translate(_WalkLeft * Time.deltaTime);
-            _BossRenderer.color = Color.red;
-            Debug.Log("step to centre");
-        }
-        else
-        {
-            Attack();
-            Debug.Log("ATTACK");
-        }
-    }
-
-    void WalkForwardR()
-    {
-        //make boss step to the right and then call attack
-        if (transform.position.x <= (_StartPoint.x - 2f))
-        {
-            transform.Translate(_WalkRight * Time.deltaTime);
-            _BossRenderer.color = Color.red;
-            Debug.Log("step to centre");
-        }
-        else
-        {
-            Attack();
-            Debug.Log("ATTACK");
-        }
-    }
-
-    void WalkBackR()
-    {
-        //make boss walk back to where it started then switch state
-        if(transform.position.x >= (_StartPoint.x))
-        {
-            
-        }
-    }
-
-    void WalkBackL()
-    {
-        //make boss walk back to where it started then switch state
-    }
-
     void Attack()
     {
         //make boss attack and then call walkback
         _BossRenderer.color = Color.black;
+        _DoneAttacking = true;
         //play attack animation, when thats over, call walkback
+    }
+
+    public override void Reason()
+    {
+
     }
 }
